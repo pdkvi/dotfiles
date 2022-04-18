d5c700c441733df4be17bf744de1af0766f5f70f
Ilya Podkovyrin <pdkvi0@gmail.com>
2022-04-19 00:39:42 +0300

    add goto-definition hotkey

--- nvim/lua/hotkeys.lua
+++ nvim/lua/hotkeys.lua
@@ -16,3 +16,5 @@ map("n", "<Leader>ff", ":Telescope find_files<CR>", opts)
 map("n", "<Leader>fg", ":Telescope live_grep<CR>", opts)
 map("n", "<Leader>fb", ":Telescope buffers<CR>", opts)
 map("n", "<Leader>fh", ":Telescope help_tags<CR>", opts)
+
+map("n", "<C-]>", ":lua vim.lsp.buf.definition()<CR>", opts)
