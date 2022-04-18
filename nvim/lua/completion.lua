b77484b7780bbc460a81a41391126d96f5bf4b90
Ilya Podkovyrin <pdkvi0@gmail.com>
2022-04-18 23:16:13 +0300

    add more language servers

--- nvim/lua/completion.lua
+++ nvim/lua/completion.lua
@@ -23,6 +23,22 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 local lspconfig = require('lspconfig')
+
 lspconfig.clangd.setup({
 	capabilities = capabilities
+})
+
+lspconfig.cmake.setup({
+	capabilities = capabilities
+})
+
+lspconfig.sumneko_lua.setup({
+	capabilities = capabilities,
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { 'vim' }
+			}
+		}
+	}
 })
