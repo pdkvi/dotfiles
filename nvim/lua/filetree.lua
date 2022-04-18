221863c4c4d415a126e843bb2f56b8327e6ad323
Ilya Podkovyrin <pdkvi0@gmail.com>
2022-04-18 21:10:39 +0300

    change git state icons for file tree

--- nvim/lua/filetree.lua
+++ nvim/lua/filetree.lua
@@ -3,5 +3,22 @@ require('nvim-tree').setup({
 		enable = true,
 		show_on_dirs = true,
 		icons = { hint = "H", info = "I", warning = "W", error = "E" }
-	}
+	},
 })
+
+-- TODO: change to lua variable
+vim.cmd([[
+let g:nvim_tree_icons =
+\ {
+	\ 'git':
+	\ {
+	\	 'unstaged': "✗",
+	\	 'staged': "✓",
+	\	 'unmerged': "⑂",
+	\	 'renamed': "→",
+	\	 'untracked': "+",
+	\	 'deleted': "-",
+	\	 'ignored': "○"
+	\ }
+\ }
+]])
