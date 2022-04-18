81cfb1f6092d45d858efc28b1fa5a482be025262
Ilya Podkovyrin <pdkvi0@gmail.com>
2022-04-19 02:30:47 +0300

    change splash screen theme

--- nvim/lua/splash.lua
+++ nvim/lua/splash.lua
@@ -1,1 +1,13 @@
-require('alpha').setup(require('alpha.themes.dashboard').config)
+local startify = require('alpha.themes.startify')
+
+startify.section.mru.val = { { type = "padding", val = 0 } }
+
+startify.section.top_buttons.val = {
+	startify.button( "f", "File Explorer" , ":NvimTreeToggle<CR>"),
+}
+
+startify.section.bottom_buttons.val = {
+	startify.button( "q", "Quit" , ":qa<CR>"),
+}
+
+require('alpha').setup(startify.config)
