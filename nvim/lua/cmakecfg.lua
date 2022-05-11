c7fce062adb92e00ce45a6717175de769d07566d
Ilya Podkovyrin <pdkvi0@gmail.com>
2022-05-11 23:26:01 +0300

    change default cmake compiler to clang

--- nvim/lua/cmakecfg.lua
+++ nvim/lua/cmakecfg.lua
@@ -5,6 +5,10 @@ require('cmake').setup({
 	parameters_file = 'neovim.json',
 	build_dir = tostring(path:new('{cwd}', 'build', '{os}-{build_type}')),
 	default_projects_path = tostring(path:new(vim.loop.os_homedir(), 'Documents/Projects')),
-	configure_args = { '-D', 'CMAKE_EXPORT_COMPILE_COMMANDS=1' },
+	configure_args = {
+		'-D', 'CMAKE_EXPORT_COMPILE_COMMANDS=1',
+		'-D', 'CMAKE_C_COMPILER=clang',
+		'-D', 'CMAKE_CXX_COMPILER=clang++'
+	},
 	quickfix_only_on_error = false
 })
