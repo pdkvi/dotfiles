49b1ffa19328c908a715c9f4e979b11328cea46d
Ilya Podkovyrin <pdkvi0@gmail.com>
2022-05-15 13:00:59 +0300

    add highlighting of trailing whitespace

--- nvim/lua/colors.lua
+++ nvim/lua/colors.lua
@@ -1,3 +1,9 @@
+vim.cmd('autocmd ColorScheme * highlight ExtraWhitespace ctermbg=red guibg=red')
+vim.cmd('autocmd BufWinEnter * match ExtraWhitespace /\\s\\+$/')
+vim.cmd('autocmd InsertEnter * match ExtraWhitespace /\\s\\+\\%#\\@<!$/')
+vim.cmd('autocmd InsertLeave * match ExtraWhitespace /\\s\\+$/')
+vim.cmd('autocmd BufWinLeave * call clearmatches()')
+
 vim.o.termguicolors = true
 vim.g.moonflyItalics = 0
 vim.cmd('colorscheme moonfly')
