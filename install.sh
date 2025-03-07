a4b38e8759129658434c3b6fe183a0efa5e56e8f
pdkvi <pdkvi0@gmail.com>
2025-03-08 02:02:45 +0300

    feat: add simple install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+ln -sr ./alacritty $HOME/.config/
+
+ln -sr ./fontconfig $HOME/.config/
+
+ln -sr ./gdb/gdbinit $HOME/.gdbinit
+
+ln -sr ./git/gitconfig $HOME/.gitconfig
+
+ln -sr ./zsh/zshrc.local $HOME/.zshrc.local
+
+ln -sr ./nvim $HOME/.config/
